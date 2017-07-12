751258c37a2b71d6e905dbd115bb0846855e5f72
Tam Thanh Tam <xitrum4692@gmail.com>
2017-07-13 00:34:48 +0700

    update - complete the framework

--- app.js
+++ app.js
@@ -1,48 +1,159 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var exphbs       = require('express-handlebars');
+var session      = require('express-session');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var methodOver   = require('method-override');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var Router       = require('named-routes');
+var Autoloader   = require('./lib/autoloader').Autoloader;
+var Auth         = require('./lib/auth').Auth;
+var PassportAuth = require('./lib/passport').Passport;
+var Errors       = require('./lib/errors');
+var db           = require('./db');
+
+// Main App
+var app = express();
+
+// Config named routes
+var router = new Router();
+router.extendExpress(app);
+router.registerAppHelpers(app);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+// Config session
+app.use(session({
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  secret: 'A77as7diubhaisdgibkn!'
+}));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Morgarn logger
 app.use(logger('dev'));
+
+// Body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// allow overriding methods in query (?_method=put)
+app.use(methodOver('_method'));
+
+// Cookie parser
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+// Public assets
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use('/admin', express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', express.static('./node_modules/admin-lte-express/public'))
-app.use('/', require('admin-lte-express'));
+//routers
+// app.use('/users', users);
+app.use('/', index);
 
-// catch 404 and forward to error handler
+// Active URL for helper
+var activeRoute = '';
+app.use(function(request, response, next) {
+  var route = router.match(request)
+  if (route) {
+    activeRoute = route.route.options.name
+  }
+  next()
+});
+
+// Session-persisted message middleware
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  var err = req.session.error;
+  var msg = req.session.success;
+  delete req.session.error;
+  delete req.session.success;
+  res.locals.message = '';
+  if (err) res.locals.message = '<p class="msg error">' + err + '</p>';
+  if (msg) res.locals.message = '<p class="msg success">' + msg + '</p>';
+  next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Auth load and middleware
+Auth.load(app);
+app.use(Autoloader.allRoutes(), Auth.restrict)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Passport load and middleware
+//PassportAuth.load(app);
+//app.use(Autoloader.allRoutes(), require('connect-ensure-login').ensureLoggedIn())
+
+// Register route
+app.get('/register', 'register', function(req, res) {
+  res.render( 'user/register', {
+    title: 'Register',
+    layout: 'auth'
+  });
 });
+app.post('/register', function(req, res) {
+  var body = req.body;
+  if (body.user) {
+    var User = new db.User({
+      id: Math.floor(100000 + Math.random() * 900000).toString().substring(0,2),
+      username: body.user.username,
+      password: body.user.password,
+      displayName: body.user.name,
+      emails: [{value: body.user.email}]
+    });
+    User.save(function(err) {
+       if (err) req.session.error = err.message;
+       else req.session.success = 'User saved successfuly!';
+       res.redirect('/register');
+    });
+  } else {
+    res.redirect('/register');
+  }
+});
+
+// MVC Autoloader
+Autoloader.load(app, {verbose: !module.parent});
+
+// Config Handlebars
+var blocks = {};
+var Handlebars = exphbs.create({
+  defaultLayout: 'main',
+  layoutsDir: __dirname + '/views/layouts/',
+  partialsDir: __dirname + '/views/partials/',
+  helpers: {
+    url: function(routeName, params) {
+      return app.locals.url(routeName, params);
+    },
+    activeRoute: function(routeName) {
+      return routeName === activeRoute ? 'active' : '';
+    },
+    activeRoutes: function(routeNames) {
+      // TODO
+      return routeNames.split(',').indexOf(activeRoute) >= 0 ? 'active' : '';
+    },
+    block: function(name) {
+      var val = (blocks[name] || []).join('\n');
+
+      // clear the block
+      blocks[name] = [];
+      return val;
+    },
+    extend: function(name, context) {
+      var block = blocks[name];
+      if (!block) {
+          block = blocks[name] = [];
+      }
+
+      block.push(context.fn(this)); // for older versions of handlebars, use block.push(context(this));
+    }
+  }
+});
+
+// View engine setup
+app.engine('handlebars', Handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+// Errors load
+Errors(app);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', {
+	      title: 'Promotion Cars',
+	      layout: 'lockscreen'
+	    });
+
 });
 
 module.exports = router;
