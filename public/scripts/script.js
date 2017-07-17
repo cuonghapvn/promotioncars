0097f42b12a18865baf10d947821fd0512cf7d21
Tam Thanh Tam <xitrum4692@gmail.com>
2017-07-17 23:37:32 +0700

    update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-  var hcmc_default = {
+  var hcm_default = {
     lat: 10.8282319,
     lng: 106.6694527,
     zoom: 13
   }
-  var dana_default = {
+  var dn_default = {
     lat: 16.0523988,
     lng: 108.2068489,
     zoom: 14
@@ -16,11 +16,29 @@ $(document).ready(function(){
     zoom: 15,
   }
 
-  new GMaps({
+  var my_map = new GMaps({
    div: '#current-position-map',
-   lat: 10.8282319,
-   lng: 106.6694527,
-   zoom: 13,
+   lat: hcm_default.lat,
+   lng: hcm_default.lng,
+   zoom: hcm_default.zoom,
+ });
+
+ $("#map-hcm-btn").on("click", function(){
+   my_map.lat = hcm_default.lat;
+   my_map.lng = hcm_default.lng;
+   my_map.zoom = hcm_default.zoom;
+ });
+
+ $("#map-dn-btn").on("click", function(){
+   my_map.lat = dn_default.lat;
+   my_map.lng = dn_default.lng;
+   my_map.zoom = dn_default.zoom;
+ });
+
+ $("#map-hn-btn").on("click", function(){
+   my_map.lat = hn_default.lat;
+   my_map.lng = hn_default.lng;
+   my_map.zoom = hn_default.zoom;
  });
 
 });
